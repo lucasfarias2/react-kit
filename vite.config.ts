eb72be3732e47ac79c05cb4493a3371d0384eabd
Lucas Farias <lukf95@gmail.com>
2023-01-07 00:33:00 +0100

    Config files

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'src',
+  // test: {
+  //   environment: 'happy-dom',
+  //   setupFiles: ['./setupVitest.js'],
+  // },
+});
