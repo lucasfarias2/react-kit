d7855ca2cd235c5bb935b389852f207930eb023d
Lucas Farias <lukf95@gmail.com>
2023-01-24 22:54:48 +0100

    Add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import analyze from 'rollup-plugin-analyzer'
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,9 +7,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    minify: false,
+    minify: true,
     emptyOutDir: true,
     rollupOptions: {
+      plugins: [analyze()],
       input: {
         home: resolve(__dirname, '/src/client/entries/home/home.html'),
       },
