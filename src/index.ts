5db3e3737e8dee9006122a79ac81167386dc1172
Lucas Farias <lukf95@gmail.com>
2023-04-08 13:16:11 +0200

    Fix production issue Vite

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { createServer as createViteServer } from 'vite';
 import type { ViteDevServer } from 'vite';
 import apiRouter from './api/router.js';
 import homeController from './server/controllers/homeController.js';
@@ -11,32 +10,33 @@ import getServerOptions from './server/serverOptions.js';
 const port = process.env.PORT || 3000;
 const hmrPort = 3001;
 
-const isProd = process.env.NODE_ENV === 'production';
-const root = process.cwd();
+(async () => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const root = process.cwd();
 
-let vite: ViteDevServer;
-const app = express();
+  let vite: ViteDevServer;
+  const app = express();
 
-app.use(renderViewMiddleware);
-app.use(deviceMiddleware);
+  app.use(renderViewMiddleware);
+  app.use(deviceMiddleware);
 
-if (isProd) {
-  app.use((await import('compression')).default());
-  app.use('/assets', express.static('dist/react/assets'));
-} else {
-  vite = await createViteServer(getServerOptions(root, hmrPort));
-  app.use(vite.middlewares);
+  if (isProd) {
+    app.use((await import('compression')).default());
+    app.use('/assets', express.static('dist/react/assets'));
+  } else {
+    vite = await (await import('vite')).createServer(getServerOptions(root, hmrPort));
+    app.use(vite.middlewares);
+    app.use((req, res, next) => {
+      res.locals.vite = vite;
+      next();
+    });
+  }
 
-  app.use((req, res, next) => {
-    res.locals.vite = vite;
-    next();
+  app.use('/api', apiRouter);
+  app.get('/', homeController.fetch, homeController.render);
+  app.use(notFoundMiddleware);
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
   });
-}
-
-app.use('/api', apiRouter);
-app.get('/', homeController.fetch, homeController.render);
-app.use(notFoundMiddleware);
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+})();
